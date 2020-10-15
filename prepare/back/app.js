2560748a233ce0af38161fd0251952865b43e439
jhpark <qkr13wlgus57@gmail.com>
2020-10-15 23:59:21 +0900

    test

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,19 +1,63 @@
 var express = require("express");
 var { graphqlHTTP } = require("express-graphql");
 var { buildSchema } = require("graphql");
+const peoples = [
+  {
+    id: "1",
+    name: "person_1",
+    age: 11,
+    gender: "female",
+  },
+  {
+    id: "2",
+    name: "person_2",
+    age: 22,
+    gender: "male",
+  },
+  {
+    id: "3",
+    name: "person_3",
+    age: 33,
+    gender: "female",
+  },
+  {
+    id: "4",
+    name: "person_4",
+    age: 44,
+    gender: "female",
+  },
+  {
+    id: "5",
+    name: "person_5",
+    age: 55,
+    gender: "male",
+  },
+];
 // var schemas = require("./graphql/sceme.graphql");
 // var resolvers = require("./graphql/resolvers");
 var schema = buildSchema(`
   type Query {
-    hello: String
+    id: String
   }
 `); // hello라는 메소드 있는데 문자열임
 
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `); // hello라는 메소드 있는데 문자열임
+
+// var root = {
+//   hello: () => {
+//     return "Hello world!";
+//   },
+// }; //hello라는 메소드
+
 var root = {
-  hello: () => {
-    return "Hello world!";
+  id: () => {
+    return peoples[0].id;
   },
-}; //hello라는 메소드
+};
 
 // test
 // query{
