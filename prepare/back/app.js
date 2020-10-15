f637bae18f297ee3589eb78d5486d7ffeefef92c
jhpark <qkr13wlgus57@gmail.com>
2020-10-16 00:00:01 +0900

    delete

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,38 +1,7 @@
 var express = require("express");
 var { graphqlHTTP } = require("express-graphql");
 var { buildSchema } = require("graphql");
-const peoples = [
-  {
-    id: "1",
-    name: "person_1",
-    age: 11,
-    gender: "female",
-  },
-  {
-    id: "2",
-    name: "person_2",
-    age: 22,
-    gender: "male",
-  },
-  {
-    id: "3",
-    name: "person_3",
-    age: 33,
-    gender: "female",
-  },
-  {
-    id: "4",
-    name: "person_4",
-    age: 44,
-    gender: "female",
-  },
-  {
-    id: "5",
-    name: "person_5",
-    age: 55,
-    gender: "male",
-  },
-];
+
 // var schemas = require("./graphql/sceme.graphql");
 // var resolvers = require("./graphql/resolvers");
 var schema = buildSchema(`
