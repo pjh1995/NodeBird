4ed28b4bf21750a0a74927152da3bab0a9a1b340
jhpark <qkr13wlgus57@gmail.com>
2020-10-17 00:21:10 +0900

    Use Saga Effect

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,1 +1,83 @@
-export default function* rootSaga();
+import { all, fork, call, take, put } from "redux-saga/effects";
+//put === dispatch
+//fork 비동기 함수 호출
+//call 동기 함수 호출
+//take 리스너
+//all addListner
+
+import axios from "axios";
+
+function logInAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+// const l = login({ type: "LOGIN_IN_REQUEST", data: { id: "jhpark@gmail.com" } });
+// l.next();1
+// test하기에 최고
+function* logIn(action) {
+  try {
+    const result = yield call(logInAPI, action.data);
+    yield put({
+      type: "LOG_IN_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_IN_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function logOutAPI() {
+  return axios.post("/api/logout");
+}
+
+function* logOut() {
+  try {
+    const result = yield call(logOutAPI);
+    yield put({
+      type: "LOG_OUT_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "LOG_OUT_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+
+function addPostAPI(data) {
+  return axios.post("/api/login", data);
+}
+
+function* addPost(action) {
+  try {
+    const result = yield call(addPostAPI, action.data);
+    yield put({
+      type: "ADD_POST_SUCCESS",
+      data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: "ADD_POST_FAILURE",
+      data: err.response.data,
+    });
+  }
+}
+function* watchLogin() {
+  yield take("LOG_IN_REQUEST", logIn);
+}
+
+function* watchLogOut() {
+  yield take("LOG_OUT_REQUEST", logOut);
+}
+
+function* watchAddPost() {
+  yield take("ADD_POST_REQUEST", addPost);
+}
+
+export default function* rootSaga() {
+  yield all([fork(watchLogin), fork(watchLogOut), fork(watchAddPost)]);
+}
