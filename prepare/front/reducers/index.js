6ee71ea04b3010128a5d673753de00dfed99d686
jhpark <qkr13wlgus57@gmail.com>
2020-10-19 00:19:49 +0900

    Delete console

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -18,7 +18,6 @@ const rootReducer = combineReducers({
   index: (state = {}, action) => {
     switch (action.type) {
       case HYDRATE: {
-        console.log("HYDRATE", action);
         return { ...state, ...action.payload };
       }
       default:
