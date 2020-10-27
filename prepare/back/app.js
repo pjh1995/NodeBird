ba06b11b5f831c58b796573f1259e56b26e84e48
jhpark <qkr13wlgus57@gmail.com>
2020-10-27 23:47:40 +0900

    라우터 쪼개기

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3065;
+const postRouter = require('./routes/post');
+/*
+    app.get -> 가져오다.
+    app.post -> 생성하다
+    app.put => 전체 수정
+    app.delete -> 제거
+    app.patch -> 부분 수정
+    app.options -> 찔러보기
+    app.head => 헤더만 가져오기(헤더/바디)
+ */
 
 app.get('/', (req, res) => {
   res.send('Hello node');
@@ -10,7 +20,7 @@ app.get('/api', (req, res) => {
   res.send('Hello nodeapi');
 });
 
-app.get('/api/posts', (req, res) => {
+app.get('/posts', (req, res) => {
   res.json([
     { id: 1, content: 'hello' },
     { id: 2, content: 'hello2' },
@@ -18,13 +28,8 @@ app.get('/api/posts', (req, res) => {
   ]);
 });
 
-app.post('/api/post', (req, res) => {
-  res.json({ id: 1, content: 'hello' });
-});
-
-app.delete('/api/post', (req, res) => {
-  res.json({ id: 1 });
-});
+//prefix
+app.use('/post', postRouter);
 
 app.listen(PORT, () => {
   console.log(`${PORT} start`);
