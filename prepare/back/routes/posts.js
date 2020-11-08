fdac58ffe65a2ab9cf87eebfd47337d1d02a8932
jhpark <qkr13wlgus57@gmail.com>
2020-11-09 00:18:10 +0900

    ADD 게시물 불러오기 기능

---
+++ prepare/back/routes/posts.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const { Post, User, Image, Comment } = require('../models');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      where: {
+        state: true,
+      },
+      limit: 10, //갯수
+      //   offset: 100, //구간
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC'],
+      ],
+      include: [
+        { model: User, attributes: ['id', 'nickname'] },
+        { model: Image },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'nickname'],
+              order: [['createdAt', 'DESC']],
+            },
+          ],
+        },
+        { model: User, as: 'Likers', attributes: ['id'] },
+      ],
+    });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
