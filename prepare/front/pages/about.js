6cb5c48440933b42389d43516b7b8c226bd07d85
jhpark <qkr13wlgus57@gmail.com>
2020-11-30 22:23:41 +0900

    change about.js

--- prepare/front/pages/about.js
+++ prepare/front/pages/about.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Head from 'next/head';
-import { END } from 'redux-saga';
+// import { END } from 'redux-saga';
 
 import AppLayout from '../components/AppLayout';
 import UserCard from '../components/UserCard';
 
-import wrapper from '../store/configureStore';
-import { LOAD_USER } from '../reducers/user';
+// import wrapper from '../store/configureStore';
+// import { LOAD_USER } from '../reducers/user';
 
 const About = () => {
   const { userInfo } = useSelector((state) => state.user);
@@ -24,13 +24,13 @@ const About = () => {
 
 // 웬만하면 안바뀌는 정보들을 브라우저 렌더링 전에 받아올 때 사용
 // html로 만들어놔 줌.
-export const getStaticProps = wrapper.getStaticProps(async (context) => {
-  context.store.dispatch({
-    type: LOAD_USER.REQUEST,
-    data: 1,
-  });
-  context.store.dispatch(END);
-  await context.store.sagaTask.toPromise();
-});
+// export const getStaticProps = wrapper.getStaticProps(async (context) => {
+//   context.store.dispatch({
+//     type: LOAD_USER.REQUEST,
+//     data: 1,
+//   });
+//   context.store.dispatch(END);
+//   await context.store.sagaTask.toPromise();
+// });
 
 export default About;
