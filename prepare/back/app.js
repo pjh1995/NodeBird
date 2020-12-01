8f160ec03bfd6aeb5a6982eb0873b133b70b5433
jhpark <qkr13wlgus57@gmail.com>
2020-12-01 23:48:55 +0900

    update cors url

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(
   cors({
-    origin: ['http://localhost:8080', 'nodebird.com'],
+    origin: ['http://localhost:8080', 'nodebird.com', 'http://13.125.239.23'],
     credentials: true,
   }),
 );
